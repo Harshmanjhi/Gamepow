635847c1e3e47f705b6692c500d74d1efd35e08a
Harshmanjhi <97271881+Harshmanjhi@users.noreply.github.com>
2023-12-26 23:21:59 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,30 +1,23 @@
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
-from telegram import Update
+from telegram.ext import Updater, CommandHandler
+from helpers import ban_user, unban_user, pin_message, unpin_message
+from your_token import TOKEN
 
-# Replace 'YOUR_BOT_TOKEN' with your actual bot token
-TOKEN = '6789771210:AAGBLOHOY7NAY19N20rJdrSoUhHu_5dLpTY'
-
-# /start command handler
-def start(update: Update, context: CallbackContext):
-    user_name = update.message.from_user.username if update.message.from_user.username else update.message.from_user.first_name
-    update.message.reply_text(f"Hello, {user_name}! I am your Telegram Bot.")
-
-# Welcome message handler
-def welcome_message(update: Update, context: CallbackContext):
-    new_chat_member = update.message.new_chat_members[0]
-    update.message.reply_text(f"Welcome {new_chat_member.first_name} to the group!")
+def start(update, context):
+    update.message.reply_text('Welcome! Use /ban <user_id> to ban a user, /unban <user_id> to unban, /pin to pin a message, and /unpin to unpin.')
 
 def main():
     updater = Updater(TOKEN, use_context=True)
     dp = updater.dispatcher
 
-    # Add handlers for commands, messages, and new chat members
     dp.add_handler(CommandHandler("start", start))
-    dp.add_handler(MessageHandler(Filters.status_update.new_chat_members, welcome_message))
+    dp.add_handler(CommandHandler("ban", ban_user, pass_args=True))
+    dp.add_handler(CommandHandler("unban", unban_user, pass_args=True))
+    dp.add_handler(CommandHandler("pin", pin_message))
+    dp.add_handler(CommandHandler("unpin", unpin_message))
 
-    # Start the bot
     updater.start_polling()
     updater.idle()
 
 if __name__ == '__main__':
     main()
+
